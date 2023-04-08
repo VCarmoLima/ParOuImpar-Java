1d0fd5087badb92d3bb59ecc38b7755944c2dd7a
Vinícius <viniciuscarmo.contato@gmail.com>
2023-04-08 18:37:42 -0300

    Incluindo uma nova Branch de teste para testar a verificação de paridade

--- ParOuImpar.java
+++ ParOuImpar.java
@@ -13,4 +13,14 @@ public class ParOuImpar {
 
     }
 
+    public static void main(String[] args) {
+        ParOuImpar parOuImpar = new ParOuImpar();
+        
+        System.out.println(parOuImpar.verificarParidade(2));
+        System.out.println(parOuImpar.verificarParidade(3));
+        System.out.println(parOuImpar.verificarParidade(10));
+        System.out.println(parOuImpar.verificarParidade(11));
+
+    }
+
 }
