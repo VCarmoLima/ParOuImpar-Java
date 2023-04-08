ac38d9e04851281dcc598ffc013fd4e45cd34d92
Vinícius <viniciuscarmo.contato@gmail.com>
2023-04-08 18:25:46 -0300

    Corpo do código

---
+++ ParOuImpar.java
@@ -0,0 +1,16 @@
+/**
+ * ParOuImpar
+ */
+public class ParOuImpar {
+
+    public String verificarParidade(int num) {
+
+        if (num % 2 == 0) {
+            return "Par";
+        } else {
+            return "Ímpar";
+        }
+
+    }
+
+}
