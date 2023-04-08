642ed7d3942a1e55f2e0eb233e68877ca5dbb932
Vinícius <viniciuscarmo.contato@gmail.com>
2023-04-08 19:12:21 -0300

    Inclusão de uma interface básica do usuário

--- ParOuImpar.java
+++ ParOuImpar.java
@@ -1,8 +1,16 @@
+import java.util.Scanner;
+
 /**
  * ParOuImpar
  */
-public class ParOuImpar {
 
+public class ParOuImpar {
+    private Scanner scanner;
+
+    public ParOuImpar() {
+        scanner = new Scanner(System.in);
+    }
+    
     public String verificarParidade(int num) {
 
         if (num % 2 == 0) {
@@ -10,7 +18,16 @@ public class ParOuImpar {
         } else {
             return "Ímpar";
         }
-
     }
+    public static void main(String[] args) {
+        ParOuImpar parOuImpar = new ParOuImpar();
 
+        System.out.print("Digite um número inteiro: ");
+        int num = parOuImpar.scanner.nextInt();
+
+        String paridade = parOuImpar.verificarParidade(num);
+
+        System.out.println("O número " + num + " é " + paridade);
+    }
+    
 }
