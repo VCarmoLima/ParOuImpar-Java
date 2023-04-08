3af93dbd3cc85ec82496383e05c6438abee6ee4c
Vinícius <viniciuscarmo.contato@gmail.com>
2023-04-08 19:22:54 -0300

    Inclusão de um loop que pergunta ao usuário se ele deseja fazer um novo teste

--- ParOuImpar.java
+++ ParOuImpar.java
@@ -21,13 +21,19 @@ public class ParOuImpar {
     }
     public static void main(String[] args) {
         ParOuImpar parOuImpar = new ParOuImpar();
+        
+        do {
+            System.out.print("Digite um número inteiro: ");
+            int num = parOuImpar.scanner.nextInt();
 
-        System.out.print("Digite um número inteiro: ");
-        int num = parOuImpar.scanner.nextInt();
+            String paridade = parOuImpar.verificarParidade(num);
 
-        String paridade = parOuImpar.verificarParidade(num);
+            System.out.println("O número " + num + " é " + paridade);
 
-        System.out.println("O número " + num + " é " + paridade);
+            System.out.print("Deseja fazer um novo teste de paridade? (Sim ou Não): ");
+        } while (parOuImpar.scanner.next().equalsIgnoreCase("Sim"));
+
+        parOuImpar.scanner.close();
     }
     
 }
